63281395fbe934c5276e49e10aadb7ead411870a
Christoph Deil <Deil.Christoph@gmail.com>
2016-01-07 19:01:48 +0100

    update

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Very old versions of Fermi-LAT high-energy TS maps.
 
 I just use those for testing, please don't use them for science!
+
+Some of the files can be generated like this:
+```
+./make.sh
+```
--- make.sh
+++ make.sh
@@ -1,1 +1,7 @@
+# https://heasarc.gsfc.nasa.gov/ftools/caldb/help/ftlist.html
 ftlist emin_0010_emax_1000.fits.gz[SIGNIFICANCE] K > header.txt
+
+# http://astropy.readthedocs.org/en/latest/visualization/index.html#scripts
+fits2bitmap emin_0010_emax_1000.fits.gz --ext 1 -o emin_0010_emax_1000.png --scale sqrt --cmap afmhot
+fits2bitmap emin_0030_emax_0100.fits.gz --ext 1 -o emin_0030_emax_0100.png --scale sqrt --cmap afmhot
+fits2bitmap emin_0100_emax_1000.fits.gz --ext 1 -o emin_0100_emax_1000.png --scale sqrt --cmap afmhot
